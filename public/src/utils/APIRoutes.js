82f964f7e9c28dd0ade0367daa90726198c4053f
Zaini-02 <zainul.haq.002@gmail.com>
2024-04-17 23:09:33 +0530

    initial commit

---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,11 @@
+export const host = "https://chat-app-backend-ueb0.onrender.com";
+
+export const registerRoute = `${host}/api/auth/register`
+export const loginRoutes = `${host}/api/auth/login`
+
+export const setAvatarRoute = `${host}/api/auth/setAvatar`
+
+export const allUsersRoute = `${host}/api/auth/allUsers`  
+
+export const sendMessageRoute = `${host}/api/messages/addmsg`
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`   
